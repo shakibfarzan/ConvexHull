26460ecb6207820d6c2dffebdcce1511509f6048
Shakib Farzan <farzan.shakib@gmail.com>
2021-11-07 08:20:40 +0330

    new package

---
+++ src/convex_hull/Main.java
@@ -0,0 +1,9 @@
+package convex_hull;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+        MainController mc = new MainController(mf);
+    }
+}
